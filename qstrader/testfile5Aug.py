db220a6fb07902ad4ae8eb3c99e184a4276e7d2a
VK <ptr2445@gmail.com>
2017-08-05 10:35:36 +0100

    'commit3'

--- qstrader/testfile5Aug.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 05 10:29:32 2017
-
-@author: User
-"""
-
-'a test'
---
+++ qstrader/tst5Aug.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 05 10:34:49 2017
+
+@author: User
+"""
+
+'a etst 5aug'
